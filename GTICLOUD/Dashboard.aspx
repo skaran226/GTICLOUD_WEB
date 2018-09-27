02e59e75315304bfdb92a0e2afae66ebb4f89b54
skaran226 <40988367+skaran226@users.noreply.github.com>
2018-09-27 18:12:29 +0530

    Add files via upload

---
+++ GTICLOUD/Dashboard.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Dashboard.aspx.cs" Inherits="GTICLOUD.Dashboard" %>
+
+<%@ Register Src="~/CssControl.ascx" TagPrefix="uc1" TagName="CssControl" %>
+<%@ Register Src="~/JsScriptControl.ascx" TagPrefix="uc1" TagName="JsScriptControl" %>
+
+
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <uc1:CssControl runat="server" ID="CssControl" />
+</head>
+<body>
+    <uc1:JsScriptControl runat="server" ID="JsScriptControl" />
+    <form id="form1" runat="server">
+    <div>
+        <h1>Welcome to Dashboard</h1>
+    </div>
+    </form>
+</body>
+</html>
---
+++ GTICLOUD/Default.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+namespace GTICLOUD
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void submit_Click(object sender, EventArgs e)
+        {
+            string query = "select email,password from registration;";
+
+            
+
+                SqlCommand cmd = DB.ExecuteReader(query);
+
+                SqlDataReader dbr = cmd.ExecuteReader();
+
+                if (!dbr.HasRows)
+                {
+
+                    Response.Write("<script>alert('No Data availabe');</script>");
+                }
+                else
+                {
+
+                    while (dbr.Read())
+                    {
+
+                        if (dbr["email"].Equals(email.Text) && dbr["password"].Equals(password.Text))
+                        {
+
+                            Response.Redirect("Dashboard.aspx");
+                        }
+                        else
+                        {
+
+                            Response.Write("<script>alert('Email or Password is incorrent');</script>");
+                            break;
+                        }
+                    }
+                }
+
+                cmd.Dispose();
+                dbr.Dispose();
+                DB.CloseConn();
+
+
+            
+        }
+
+
+
+    }
+}
